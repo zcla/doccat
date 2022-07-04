b4bbea222c25bc5274d5e7305210374bb8670c4d
zcla <zeclaudioaguiar@gmail.com>
2022-07-03 23:47:57 -0300

    Links para página e catecismo.grupo

--- html/js/doccat.js
+++ html/js/doccat.js
@@ -1,9 +1,12 @@
 "use strict";
 
-function loadHtml(arquivo, selector) {
+function loadHtml(arquivo, selector, callback) {
     $(selector).empty();
     $(selector).load(arquivo, function() {
         refReplace(selector);
+        if (callback) {
+            callback();
+        }
     });
 }
 
@@ -13,10 +16,10 @@ function refReplace(selector) {
         $(this).replaceWith(replacement);
     });
 
-    $('ref-catecismodetalhe').each(function() {
+    $('ref-catecismogrupo').each(function() {
         let name = $(this).attr('name');
         name = name ? name : this.innerText;
-        const replacement = $('<a onclick="javascript:Catecismo.detalhe(\'' + name + '\');">').append(this.innerHTML);
+        const replacement = $('<a href="?pagina=catecismo&grupo=' + name + '\">').append(this.innerHTML);
         $(this).replaceWith(replacement);
     });
 
@@ -24,7 +27,7 @@ function refReplace(selector) {
         let name = $(this).attr('name');
         name = name ? name : this.innerText;
         let replacement = $('<a onclick="javascript:Catecismo.referencia(\'' + name + '\');">').append(this.innerHTML);
-        if (selector == '#detalhe') { // Se o link vier da barra de detalhe, colocar na estrutura do texto
+        if (selector == '#grupo') { // Se o link vier da barra de grupo, colocar na estrutura do texto
             replacement = $('<a onclick="javascript:Catecismo.texto(\'' + name + '\');">').append(this.innerHTML);
         }
         $(this).replaceWith(replacement);
@@ -33,29 +36,34 @@ function refReplace(selector) {
 
 $(document).ready(function () {
     // Trata parâmetros na URL
-    const params = location.search.replace('?', '').split('&');
-    for (const param of params) {
-        const [key, value] = param.split('=')
-        switch (key) {
-            case "pagina":
-                loadHtml(value + '.html', '#doccat');
-                break;
-            case "detalhe":
-                // TODO Fazer só quando "pagina=catecismo"
-                setTimeout(function() {
-                    Catecismo.detalhe(value);
-                }, 100); // TODO deixar mais "profissional" (carregar depois de loadHtml() terminar)
-                break;
-            default:
-                throw "Parâmetro desconhecido";
-        }
+    const paramArray = location.search.replace('?', '').split('&');
+    const params = {};
+    for (const param of paramArray) {
+        const [key, value] = param.split('=');
+        params[key] = value;
+    }
+    if (params.pagina) {
+        loadHtml(params.pagina + '.html', '#doccat', function() {
+            switch (params.pagina) {
+                case 'catecismo':
+                    if (params.grupo) {
+                        Catecismo.grupo(params.grupo);
+                    }
+                    break;
+                case 'tribos':
+                    // Nada
+                    break;
+                default:
+                    break;
+            }
+        });
     }
 });
 
 class Catecismo {
-    // "Detalhe" da estrutura do catecismo (subestrutura de um trecho)
-    static detalhe(nome) {
-        loadHtml('catecismo/' + nome + '.html', '#detalhe');
+    // "Grupo" da estrutura do catecismo (subestrutura de um trecho)
+    static grupo(nome) {
+        loadHtml('catecismo/' + nome + '.html', '#grupo');
     }
 
     // Mostra o texto como referência
