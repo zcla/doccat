a4f79f24f186f5e0d8afe65858a27153bd6a5507
zcla <zeclaudioaguiar@gmail.com>
2022-09-06 23:46:54 -0300

    new file:   powershell/json2html/Biblia_clerus_pt.ps1 	modified:   powershell/json2html/Biblia_vatican_lt.ps1 	modified:   powershell/json2html/_Tudo.ps1

--- powershell/json2html/Biblia_vatican_lt.ps1
+++ powershell/json2html/Biblia_vatican_lt.ps1
@@ -15,76 +15,76 @@ $idBiblia = $id.replace('Biblia_', '')
 
 $fileName = "$prjPath\html\biblia"
 ForEach ($objLivro In $biblia.ordem) {
-    $sigla = $objLivro.sigla
-    Write-Host "  $sigla" -ForegroundColor Cyan -NoNewline
-    $livro = $biblia.livros.$sigla
-    $configLivro = $config.biblia.livro | Where-Object { $_.sigla -eq $sigla }
-    $htmlLivro = @"
+	$sigla = $objLivro.sigla
+	Write-Host "  $sigla" -ForegroundColor Cyan -NoNewline
+	$livro = $biblia.livros.$sigla
+	$configLivro = $config.biblia.livro | Where-Object { $_.sigla -eq $sigla }
+	$htmlLivro = @"
 <div id="$sigla">
 	<p class="nome">$($configLivro.nomeLongo)</p>
 "@
-    If ($objLivro.capitulos.Count -gt 1) {
-        $htmlLivro += @"
+	If ($objLivro.capitulos.Count -gt 1) {
+		$htmlLivro += @"
 
 	<p class="capitulos">
 "@
-    }
-    ForEach ($objCapitulo In $objLivro.capitulos) {
-        $numCapitulo = $objCapitulo.capitulo
-        If ($numCapitulo -match '^#') {
-            Continue
-        }
-        If ($numCapitulo -ne '-') {
-            $htmlLivro += @"
+	}
+	ForEach ($objCapitulo In $objLivro.capitulos) {
+		$numCapitulo = $objCapitulo.capitulo
+		If ($numCapitulo -match '^#') {
+			Continue
+		}
+		If ($numCapitulo -ne '-') {
+			$htmlLivro += @"
 
-        <a href="?pagina=biblia&amp;livro=$sigla&amp;capitulo=$numCapitulo">$numCapitulo</a>
+		<a href="?pagina=biblia&amp;livro=$sigla&amp;capitulo=$numCapitulo">$numCapitulo</a>
 "@
-        }
-        Write-Host " $numCapitulo" -ForegroundColor Cyan -NoNewline
-        $capitulo = $livro.capitulos.$numCapitulo
-        $htmlCapitulo = @"
+		}
+		Write-Host " $numCapitulo" -ForegroundColor Cyan -NoNewline
+		$capitulo = $livro.capitulos.$numCapitulo
+		$htmlCapitulo = @"
 <div>
 "@
-        If ($numCapitulo -ne '-') {
-            $htmlCapitulo += @"
+		If ($numCapitulo -ne '-') {
+			$htmlCapitulo += @"
 
-    <span class="capitulo">$numCapitulo</span>
+	<span class="capitulo">$numCapitulo</span>
 "@
 
-        }
-        ForEach ($numVersiculo In $objCapitulo.versiculos) {
-            If ($numCapitulo -eq '-') {
-                $htmlLivro += @"
+		}
+		ForEach ($numVersiculo In $objCapitulo.versiculos) {
+			If ($numCapitulo -eq '-') {
+				$htmlLivro += @"
 
-    <span class="versiculo"><sup>$numVersiculo</sup> $($capitulo.versiculos.$numVersiculo)</span>
+	<span class="versiculo"><sup>$numVersiculo</sup> $($capitulo.versiculos.$numVersiculo)</span>
 "@
-            } Else {
-                $htmlCapitulo += @"
+			} Else {
+				$htmlCapitulo += @"
 
-    <span class="versiculo"><sup>$numVersiculo</sup> $($capitulo.versiculos.$numVersiculo)</span>
+	<span class="versiculo"><sup>$numVersiculo</sup> $($capitulo.versiculos.$numVersiculo)</span>
 "@
-            }
-        }
-        $htmlCapitulo += @"
+			}
+		}
+		$htmlCapitulo += @"
 
 </div>
 "@
-        $fileName = "$prjPath\html\biblia\$idBiblia\$sigla\$numCapitulo\index.html"
-        If ($numCapitulo -ne '-') {
-            $htmlCapitulo | Out-File (New-Item $fileName -Force)
-        }
-    }
-    If ($objLivro.capitulos.Count -gt 1) {
-        $htmlLivro += @"
+		$fileName = "$prjPath\html\biblia\$idBiblia\$sigla\$numCapitulo\index.html"
+		If ($numCapitulo -ne '-') {
+			$htmlCapitulo | Out-File (New-Item $fileName -Force)
+		}
+	}
+	If ($objLivro.capitulos.Count -gt 1) {
+		$htmlLivro += @"
 
-    </p>
+	</p>
 "@
-    }
-    $htmlLivro += @"
+	}
+	$htmlLivro += @"
 
 </div>
 "@
-    $fileName = "$prjPath\html\biblia\$idBiblia\$sigla\index.html"
-    $htmlLivro | Out-File (New-Item $fileName -Force)
-    Write-Host ""
+	$fileName = "$prjPath\html\biblia\$idBiblia\$sigla\index.html"
+	$htmlLivro | Out-File (New-Item $fileName -Force)
+	Write-Host ""
 }
