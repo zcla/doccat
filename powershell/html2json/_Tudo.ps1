17289efa7f26d97c5edc75075c1e13aaf045f892
zcla <zeclaudioaguiar@gmail.com>
2022-09-06 23:46:50 -0300

    Biblia_clerus_pt: html2json

--- powershell/html2json/_Tudo.ps1
+++ powershell/html2json/_Tudo.ps1
@@ -2,3 +2,8 @@ Clear-Host
 
 Write-Host "Biblia_vatican_lt" -ForegroundColor Magenta
 .\Biblia_vatican_lt.ps1
+
+Write-Host "Biblia_clerus_pt" -ForegroundColor Magenta
+.\Biblia_clerus_pt.ps1
+
+# TODO Criar script para comparar bíblias e mostrar as diferenças (ou, melhor, fazer em JavaScript)
