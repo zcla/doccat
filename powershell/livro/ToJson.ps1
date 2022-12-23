ad050d863f61536eef231dfcfec10bbb878fed5d
zcla <zeclaudioaguiar@gmail.com>
2022-12-23 12:36:24 -0300

    Livro: A Constância

--- powershell/livro/ToJson.ps1
+++ powershell/livro/ToJson.ps1
@@ -1,61 +1,28 @@
+Set-Location $PSScriptRoot
+
 $result = [ordered]@{
-    id = "AAutoEstimaDoCristao"
-    titulo = "A Auto-estima do Cristão"
-    autor = "Michel Esparza"
+    id = "AConstancia"
+    titulo = "A Constância"
+    autor = "Rafael Llano Cifuentes"
     editora = "Quadrante"
-    ano = "2008"
+    ano = "2019"
     textoEstrutura = @"
-Introdução	5
-Primeira parte - Os problemas do eu	11
- 	O ser humano em busca da sua dignidade	13
- 	 	A fonte de todos os problemas	13
- 	 	O maior de todos os problemas	15
- 	 	A origem da soberba	17
- 	 	Três estágios da vida	20
- 	 	O orgulho é uma mentira vital	23
- 	 	O orgulho é competitivo e causa cegueira	26
- 	 	Orgulho até na vida cristã: o perfeccionismo	30
- 	 	A vaidade	35
- 	Personalidade e afetividade: independência e dependência	40
- 	 	As energias do coração	41
- 	 	Afeto e desprendimento	44
- 	 	Sensíveis e fortes	47
- 	 	Conjugar dependência e independência	49
- 	 	Liberdade interior e humildade	52
- 	Auto-estima humilde ou auto-estima orgulhosa?	55
- 	 	Diversos enfoques da auto-estima	55
- 	 	Duas atitudes diante de nós mesmos	58
- 	 	Egoísmo e egocentrismo	62
- 	 	Onde começa a neurose?	64
-Segunda parte - Pautas para uma possível solução	69
- 	Humildade e verdade	71
- 	 	Humildade e auto-estima	71
- 	 	A humildade evita tanto a arrogância como a auto-rejeição	74
- 	 	O esquecimento próprio e a falsa humildade	76
- 	 	A verdadeira humildade e liberdade do cristão	78
- 	 	Liberdade e entrega por amor	82
- 	A purificação do coração	85
- 	 	Mudar os alicerces da personalidade	85
- 	 	Pecados e virtudes	87
- 	 	A graça que dignifica e sara	89
- 	Só o amor de Deus oferece soluções estáveis	92
- 	 	"Respeitos humanos" e "respeitos divinos"	92
- 	 	Uma vida inteira à procura do que já se tem	94
- 	 	Não é fácil situar-se na perspectiva do amor de Deus	101
- 	 	O filho mais velho da parábola	103
- 	Diversas manifestações do amor de Deus	107
- 	 	Filiação divina	107
- 	 	Amizade recíproca com Cristo	110
- 	 	Valemos todo o sangue de Cristo	118
- 	O amor misericordioso	124
- 	 	O que significa ser misericordioso?	126
- 	 	Cristo revela a misericórdia divina	129
- 	 	Pode-se estar orgulhoso da própria fraqueza?	131
- 	 	Duas condições: amor recíproco e boa vontade	135
- 	 	Vida de infância espiritual	142
- 	 	Admiráveis perspectivas de futuro	147
-Epílogo: "ama-me tal como és"	153
-Índice	157
+Sumário	3
+Uma condição indispensável	5
+ 	Não há constância sem ideal	5
+ 	O ideal não se realiza sem constância	8
+ 	A constância e o desenvolvimento da vida cristã	11
+Os caminhos da constância	19
+ 	Domínio do temperamento	20
+ 	Vencer inclinações, costumes e hábitos	25
+ 	A luta contra a vaidade	27
+ 	A nossa verdadeira imagem	29
+ 	A superação do sensível	35
+ 	A diligência e a laboriosidade	39
+ 	Ultrapassar os obstáculos com espírito esportivo	43
+Duas palavras finais	51
+ 	Confiar	51
+ 	Amar	55
 "@
         estrutura = @()
 }
