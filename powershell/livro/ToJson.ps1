6367cb9910c5dbf5732a091d22a39ca084cabfaf
zcla <zeclaudioaguiar@gmail.com>
2023-02-07 11:52:18 -0300

    Livro: Tratado da Oração e da Meditação

--- powershell/livro/ToJson.ps1
+++ powershell/livro/ToJson.ps1
@@ -1,44 +1,77 @@
 Set-Location $PSScriptRoot
 
 $result = [ordered]@{
-    id = "AsTresViasEAsTresConversoes"
-    titulo = "As Três Vias e as Três Conversões"
-    autor = "Réginald Garrigou Lagrange"
-    editora = "Permanência"
-    ano = "2011"
+    id = "TratadoDaOracaoEDaMeditacao"
+    titulo = "Tratado da Oração e da Meditação"
+    autor = "São Pedro de Alcântara"
+    editora = "Editora Vozes"
+    ano = "2013"
     textoEstrutura = @"
-Sumário	7
-Prefácio	9
-Prólogo	11
-Capítulo I - A vida da graça e o valor da primeira conversão	15
- 	Necessidade da vida interior	15
- 	Qual é o princípio ou a fonte da vida interior?	18
- 	A realidade da graça e de nossa filiação divina adotiva	21
- 	A vida eterna começada	24
- 	O valor da verdadeira conversão	28
- 	As três idades da vida espiritual	36
-Capítulo II - A segunda conversão: entrada na via iluminativa	41
- 	A segunda conversão dos apóstolos	43
- 	Como deve ser nossa segunda conversão - as imperfeições que a tornam necessária	46
- 	Os principais motivos que devem inspirar a segunda conversão e quais são seus frutos	50
-Capítulo III - A terceira conversão: ou transformação da alma, entrada na via unitiva dos perfeitos	57
- 	A descida do Espírito Santo sobre os apóstolos	58
- 	Quais foram os efeitos da descida do Espírito Santo?	60
- 	A purificação do espírito, necessária à perfeição cristã	65
- 	A necessidade da purificação do espírito	66
- 	Como Deus purifica a alma no momento desta terceira conversão ou transformação?	68
- 	Oração ao Espírito Santo	73
- 	Consagração e oração ao Espírito Santo	73
-Nota do editor explicando que o capítulo IV foi omitido desta edição a conselho do autor	74
-Capítulo V - Características de cada uma das fases da vida espiritual	75
- 	A fase dos principiantes	76
- 	A fase dos avançados	81
- 	A fase dos perfeitos	86
-Capítulo VI - A paz no reino de Deus, prelúdio da vida no céu	89
- 	O despertar divino	89
- 	A viva chama	91
- 	Pax in veritate	95
-Nota final - O chamado à contemplação infusa dos mistérios da fé	97
+Sumário	5
+À guisa de prefácio	9
+Notícia biográfica sobre São Pedro de Alcântara	13
+Primeira parte - Tratado da Oração e da Meditação	29
+ 	Capítulo I - Do fruto que se tira da oração e da meditação	31
+ 	Capítulo II - Da matéria da meditação	34
+ 	 	Segunda-feira	35
+ 	 	Terça-feira	38
+ 	 	Quarta-feira	42
+ 	 	Quinta-feira	46
+ 	 	Sexta-feira	48
+ 	 	Sábado	51
+ 	 	Domingo	55
+ 	Capítulo III - Do tempo e fruto destas meditações sobreditas	58
+ 	Capítulo IV - Das outras sete meditações da Sagrada Paixão e da maneira que havemos de ter em meditá-la	59
+ 	 	Segunda-feira	61
+ 	 	 	Da instituição do Santíssimo Sacramento	63
+ 	 	Terça-feira	64
+ 	 	Quarta-feira	67
+ 	 	Quinta-feira	71
+ 	 	Sexta-feira	74
+ 	 	Sábado	79
+ 	 	Domingo	81
+ 	Capítulo V - De seis coisas que podem intervir no exercício da oração	85
+ 	Capítulo VI - Da preparação que se requer para antes da oração	87
+ 	Capítulo VII - Da leitura	89
+ 	Capítulo VIII - Da meditação	90
+ 	Capítulo IX - Da ação de graças	91
+ 	Capítulo X - Do oferecimento	93
+ 	Capítulo XI - Da petição	95
+ 	 	Petição especial do amor de Deus	97
+ 	Capítulo XII - De alguns avisos que se devem ter neste santo exercício	101
+ 	 	Primeiro aviso	101
+ 	 	Segundo aviso	102
+ 	 	Terceiro aviso	102
+ 	 	Quarto aviso	103
+ 	 	Quinto aviso	104
+ 	 	Sexto aviso	105
+ 	 	Sétimo aviso	106
+ 	 	Oitavo aviso	107
+Segunda parte - Tratado que fala da devoção	111
+ 	Capítulo I - Que coisa seja a devoção	113
+ 	Capítulo II - De nove coisas que ajudam a alcançar a devoção	116
+ 	Capítulo III - De dez coisas que impedem a devoção	118
+ 	Capítulo IV - Das tentações mais comuns que costumam fatigar os que se dão à oração e seus remédios	120
+ 	 	Primeiro aviso	120
+ 	 	Segundo aviso	122
+ 	 	Terceiro aviso	122
+ 	 	Quarto aviso	123
+ 	 	Quinto aviso	123
+ 	 	Sexto aviso	124
+ 	 	Sétimo aviso	124
+ 	 	Oitavo aviso	125
+ 	 	Nono aviso	125
+ 	Capítulo V - De alguns avisos necessários para os que se dão à oração	127
+ 	 	Primeiro aviso	127
+ 	 	Segundo aviso	129
+ 	 	Terceiro aviso	130
+ 	 	Quarto aviso	130
+ 	 	Quinto aviso	130
+ 	 	Sexto aviso	131
+ 	 	Sétimo aviso	132
+ 	 	Oitavo aviso	132
+Breve introdução - Para os que começam a servir a Nosso Senhor	135
+De três coisas que deve fazer quem quiser aproveitar muito em pouco tempo	140
 "@
         estrutura = @()
 }
@@ -93,6 +126,15 @@ foreach ($texto in $spl) {
     }
 }
 $result.Remove('textoEstrutura')
+
+@"
+`t`t`t`t`t<tr>
+`t`t`t`t`t`t<td><a href="?pagina=livro&id=$($result.id)">$($result.titulo)</a></td>
+`t`t`t`t`t`t<td>$($result.autor)</td>
+`t`t`t`t`t`t<td>$($result.editora)</td>
+`t`t`t`t`t`t<td class="text-end">$($result.ano)</td>
+`t`t`t`t`t</tr>
+"@ | Set-Clipboard
 
 $outPath = (Get-Item -Path "$PSScriptRoot\..\..").FullName
 $outFile = "$outPath\html\livro\$($result.id).json"
